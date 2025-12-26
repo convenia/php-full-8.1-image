b4a6e615679e13bf77734f39db792077a7d3a960
Leonardo Lemos <leonardo.lemos@convenia.com.br>
2025-12-26 12:26:18 -0300

    fix new PHP image

--- public/index.php
+++ public/index.php
@@ -38,11 +38,11 @@
         .button {
             display: block;
             width: 190px;
-            border-radius: 20px;
-            background: linear-gradient(135deg, #BC4CF7 0%, #7D71EF 100%);
-            color: #FFFFFF;
-            font-family: Ubuntu;
-            font-size: 12px;
+            border-radius: 10px;
+            background: linear-gradient(135deg, #A3D91A 0%, #F8FDF1 100%);
+            color: #121E48;
+            font-family: 'Nunito Sans', sans-serif;
+            font-size: 14px;
             line-height: 16px;
             text-align: center;
             text-decoration: none;
@@ -63,7 +63,7 @@
             height: 4px;
             width: 100px;
             border-radius: 4px;
-            background: linear-gradient(135deg, #BC4CF7 0%, #7D71EF 100%);
+            background: linear-gradient(135deg, #96ce00 0%, #A3D91A 100%);
         }
 
         .mt40 {
@@ -71,12 +71,12 @@
         }
 
         /* pre {
-            background: linear-gradient(135deg, #BC4CF7 0%, #7D71EF 100%);
+            background: linear-gradient(135deg, #96ce00 0%, #8FC400 100%);
             color: #fff;
         } */
     </style>
 </head>
-<body style="margin: 0; background-color: #fff">
+<body style="margin: 5% 0 0 0; background-color: #fff">
     <div style="max-width: 600px; margin: 0 auto;">
         <table
             cellpadding="0"
@@ -87,13 +87,7 @@
                 border: 0; border-collapse: collapse;"
         >
             <tr>
-                <td>
-                    <img
-                        src="https://images-convenia.s3.amazonaws.com/email/convenia.png"
-                        alt="convenia-logo"
-                        style="padding: 60px 0 62px 40px;"
-                    >
-        
+                <td>        
                     <h1 style="margin-left: 40px;">
                         <small>
                             Convenia PHP Full Image
@@ -104,12 +98,29 @@
                     </div>
                 </td>
         
-                <td style="text-align: right; line-height: normal;">
-                    <img
-                        src="https://images-convenia.s3.amazonaws.com/email/background.png"
-                        alt="convenia-logo"
-                        style="line-height: normal; margin-top: -3px;"
-                    >
+                <td style="text-align: right; line-height: normal; padding-right: 40px;">                    
+                    <svg width="230" height="80" viewBox="0 0 115 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0_299_1562)">
+                    <path d="M34.5309 13.8857C31.066 13.8857 28.3562 16.5956 28.3562 20.0267C28.3562 23.4578 31.066 26.2014 34.5309 26.2014C37.9958 26.2014 40.6719 23.4916 40.6719 20.0267C40.6719 16.5618 37.962 13.8857 34.5309 13.8857ZM34.5309 23.594C32.5413 23.594 31.0998 22.0849 31.0998 20.0256C31.0998 17.9663 32.5402 16.492 34.5309 16.492C36.5216 16.492 37.9272 18.0011 37.9272 20.0256C37.9272 22.0501 36.4867 23.594 34.5309 23.594Z" fill="#13164E" fill-opacity="0.95"/>
+                    <path d="M100.3 12.4791C101.088 12.4791 101.844 11.7927 101.844 10.9352C101.844 10.0776 101.088 9.35742 100.3 9.35742C99.5107 9.35742 98.7556 10.0439 98.7556 10.9352C98.7556 11.8264 99.4758 12.4791 100.3 12.4791Z" fill="#13164E" fill-opacity="0.95"/>
+                    <path d="M109.463 13.9882C107.886 13.9882 106.273 14.4001 104.9 15.1551L106.032 17.2482C106.993 16.734 108.263 16.3907 109.222 16.3907C110.869 16.3907 111.864 17.3855 111.864 18.449V18.4828L107.953 19.1343C105.826 19.58 104.282 20.7121 104.282 22.8738C104.282 25.0356 106.238 26.3049 108.193 26.3049C109.77 26.3049 111.418 25.4474 112.001 24.1781L112.172 26.0652H114.607V18.5863C114.607 15.9789 112.583 13.9893 109.461 13.9893L109.463 13.9882ZM111.865 20.7807C111.865 22.5644 110.595 24.0397 108.914 24.0397C107.884 24.0397 106.993 23.4567 106.993 22.5644C106.993 21.672 107.851 21.1926 108.743 21.0204L111.865 20.4375V20.7807Z" fill="#13164E" fill-opacity="0.95"/>
+                    <path d="M81.6373 20.0953C81.6373 16.3907 79.0986 13.9893 75.8734 13.9893C72.4085 13.9893 69.7673 16.7339 69.7673 20.3361C69.7673 24.2467 72.9239 26.2711 76.2853 26.2711C77.9665 26.2711 79.8875 25.7568 81.0195 24.8645L79.8526 22.7714C79.0637 23.3543 77.7595 23.8348 76.3528 23.8348C74.6378 23.8348 72.9217 23.0797 72.6482 21.1588H81.499C81.6014 20.7818 81.6363 20.541 81.6363 20.0953H81.6373ZM72.5458 19.203C72.889 17.6939 73.9525 16.3896 75.8048 16.3896C77.4174 16.3896 78.5832 17.5217 78.7892 19.203H72.5458Z" fill="#13164E" fill-opacity="0.95"/>
+                    <path d="M46.4435 26.0641H43.665V14.2628H46.0665L46.3749 16.0126C46.924 15.0516 48.3307 14.023 50.0457 14.023C53.0301 14.023 54.6089 16.3559 54.6089 19.1343V26.0641H51.8304V19.3054C51.8304 18.1047 51.1102 16.698 49.3266 16.698C47.6453 16.698 46.4446 17.9674 46.4446 19.751V26.063L46.4435 26.0641Z" fill="#13164E" fill-opacity="0.95"/>
+                    <path d="M87.2846 26.0641H84.5061V14.2628H86.9076L87.2159 16.0126C87.7651 15.0516 89.1717 14.023 90.8868 14.023C93.8711 14.023 95.45 16.3559 95.45 19.1343V26.0641H92.6715V19.3054C92.6715 18.1047 91.9513 16.698 90.1676 16.698C88.4864 16.698 87.2857 17.9674 87.2857 19.751V26.063L87.2846 26.0641Z" fill="#13164E" fill-opacity="0.95"/>
+                    <path d="M101.603 14.1603H98.9956V25.9617H101.603V14.1603Z" fill="#13164E" fill-opacity="0.95"/>
+                    <path d="M63.8213 25.9617H60.7672L56.2051 14.1603H59.1895L62.3111 22.7365H62.3798L65.5015 14.1603H68.4183L63.8213 25.9617Z" fill="#13164E" fill-opacity="0.95"/>
+                    <path d="M24.6609 23.9035C23.4601 25.1728 22.0197 25.9966 20.029 25.9966C16.5292 25.9966 14.0254 23.3892 14.0254 20.0615C14.0254 16.7339 16.6328 14.1603 20.029 14.1603C21.8813 14.1603 23.4253 14.8468 24.5574 16.1499L26.8215 14.2627C25.2089 12.4104 22.7737 11.2784 20.029 11.2784C14.9864 11.2784 11.0061 15.0178 11.0061 20.0605C11.0061 25.1031 14.8142 28.8774 20.029 28.8774C23.082 28.8774 25.3462 27.4708 26.8564 25.893L24.6609 23.9035Z" fill="#13164E" fill-opacity="0.95"/>
+                    <path d="M29.1163 27.7836C26.9153 30.3583 23.6443 31.9938 19.9986 31.9938C13.3858 31.9938 8.00541 26.6134 8.00541 20.0006C8.00541 13.3879 13.3858 8.00749 19.9986 8.00749C23.6367 8.00749 26.9 9.63534 29.1021 12.2013L30.9849 10.5866C28.3285 7.49102 24.3885 5.52649 19.9996 5.52649C12.0184 5.52649 5.52441 12.0194 5.52441 20.0017C5.52441 27.9841 12.0173 34.4769 19.9996 34.4769C24.3951 34.4769 28.3383 32.507 30.9958 29.4049L29.1173 27.7858L29.1163 27.7836Z" fill="#96CE00"/>
+                    <path d="M33.3045 31.3932C30.088 35.1447 25.3167 37.5254 19.9984 37.5254C19.9733 37.5254 19.9472 37.5254 19.9221 37.5254V39.9999C19.9472 39.9999 19.9733 39.9999 19.9984 39.9999C26.0641 39.9999 31.5067 37.2857 35.1775 33.008L33.3045 31.3932Z" fill="#96CE00"/>
+                    <path d="M6.67495 8.62848L4.79759 7.01807C1.80775 10.5146 -0.000976562 15.0494 -0.000976562 20.0006C-0.000976562 24.9517 1.81428 29.5029 4.81502 33.0027L6.69239 31.3922C4.06429 28.3261 2.47458 24.3458 2.47458 20.0006C2.47458 15.6553 4.05775 11.6924 6.67604 8.62848H6.67495Z" fill="#96CE00"/>
+                    <path d="M19.9996 0C19.9746 0 19.9484 0 19.9233 0V2.47446C19.9484 2.47446 19.9746 2.47446 19.9996 2.47446C25.3135 2.47446 30.0827 4.85304 33.2992 8.60015L35.1765 6.98973C31.5057 2.71308 26.0632 0 19.9996 0Z" fill="#96CE00"/>
+                    </g>
+                    <defs>
+                    <clipPath id="clip0_299_1562">
+                    <rect width="114.609" height="40" fill="white"/>
+                    </clipPath>
+                    </defs>
+                    </svg>
                 </td>
             </tr>
         </table>
@@ -134,7 +145,6 @@
             </p>
 
             <pre class="button code">
-version: '3.3'
 services:
   app:
     image: convenia/php-full:latest
@@ -158,7 +168,8 @@ services:
             </p>
 
             <pre class="button code">
-RUN addgroup -S -g 2000 newone && adduser -u 2000 -G newone -D newone && \
+RUN addgroup -S -g 2000 newone && \
+    adduser -u 2000 -G newone -D newone && \
     sed -i 's/app/newone/g' /usr/local/etc/php-fpm.d/www.conf && \
     chown -R newone:newone /var/www/app</pre>
 
@@ -195,7 +206,6 @@ COPY ./local-path-to-config/new-default.conf /etc/nginx/http.d/default.conf</pre
 
             <p>Or we can override the default configuration at mount time:</p>
             <pre class="button code">
-version: '3.3'
 services:
   app:
     image: convenia/php-full
@@ -256,15 +266,15 @@ docker run --rm easycron crond -l 2 -f
             </div>
 
             <p>
-                For any suggestion or bug try to open an issue on github or send a tweet to us
+                For any suggestion or bug try to open an issue on github or contact the image maintainer directly.
             </p>
 
             <a href="https://github.com/convenia/php-full-8.1-image" class="button"  style="display: inline-block;">
                 Github
             </a>
 
-            <a target="_BLANK" href="https://twitter.com/convenia" class="button" style="display: inline-block;">
-                Twitter
+            <a target="_BLANK" href="https://leonardolemos.com.br/" class="button" style="display: inline-block;">
+                Maintainer Contact
             </a>
 
             <p></p>
@@ -277,7 +287,7 @@ docker run --rm easycron crond -l 2 -f
             
             <img
                 width="100px"
-                src="https://images-convenia.s3.amazonaws.com/email/logo.png"
+                src="https://images-convenia.s3.us-east-1.amazonaws.com/email/convenia-logo-green.png"
                 alt="convenia-logo-small"
                 style="display: block; margin: 20px auto;"
             >            
